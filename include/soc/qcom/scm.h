5f91888fefc73b19e5162e863b3ab424f2fd547a
Yahya Wessam <yahyawessam2002@gmail.com>
2026-06-07 23:34:36 +0300

    Revert "memlat: Optimize perf event reads when possible"
    This reverts commit 3c2b543f0031e6596d50df2b71693f78d4365068.

--- include/soc/qcom/scm.h
+++ include/soc/qcom/scm.h
@@ -124,7 +124,7 @@ struct scm_hdcp_req {
 };
 
 extern struct mutex scm_lmh_lock;
-extern bool under_scm_call(int cpu);
+extern bool under_scm_call(void);
 
 #else
 
@@ -188,7 +188,7 @@ static inline int scm_enable_mem_protection(void)
 	return 0;
 }
 
-extern bool under_scm_call(int cpu)
+extern bool under_scm_call(void)
 {
 	return false;
 }
